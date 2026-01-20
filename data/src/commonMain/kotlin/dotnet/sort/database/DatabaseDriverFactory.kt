c90e0894a15dcf523d46a29d331617c047f18af5
seg <segnities007@gmail.com>
2026-01-21 06:50:58 +0900

    modify

--- data/src/commonMain/kotlin/dotnet/sort/database/DatabaseDriverFactory.kt
+++ data/src/commonMain/kotlin/dotnet/sort/database/DatabaseDriverFactory.kt
@@ -1,9 +1,6 @@
 package dotnet.sort.database
 
 import app.cash.sqldelight.db.SqlDriver
-import org.koin.core.annotation.Single
-
-@Single
 expect class DatabaseDriverFactory {
     fun createDriver(): SqlDriver
 }
