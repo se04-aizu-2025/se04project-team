5569e4b84efe2101a5adb68b5426a8eaa84de617
seg <segnities007@gmail.com>
2026-01-23 16:56:50 +0900

    modifyu

--- presentation/feature/compare/src/commonMain/kotlin/dotnet/sort/presentation/feature/compare/CompareScreen.kt
+++ presentation/feature/compare/src/commonMain/kotlin/dotnet/sort/presentation/feature/compare/CompareScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -117,7 +119,9 @@ fun CompareScreen(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(SpacingTokens.M),
+                    .padding(padding)
+                    .padding(SpacingTokens.M)
+                    .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 SortText(
